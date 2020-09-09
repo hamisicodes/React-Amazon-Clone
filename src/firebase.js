403b271d109390c83de721a9e3d6500811255f44
hamisicodes <newhamisi@gmail.com>
2020-09-09 23:18:52 +0300

    Implement and style the Header component 🚀

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD0D-al_OmChL3g9ZVUv495WaOMwvcEeXA",
+    authDomain: "clone-6a95a.firebaseapp.com",
+    databaseURL: "https://clone-6a95a.firebaseio.com",
+    projectId: "clone-6a95a",
+    storageBucket: "clone-6a95a.appspot.com",
+    messagingSenderId: "917476144299",
+    appId: "1:917476144299:web:0aea4ed1025f15bfdeb3ee"
+  };
