93124d6b3ee87b5ad2986391d0aa1da389751c79
hamisicodes <newhamisi@gmail.com>
2020-09-14 11:05:06 +0300

    create the Checkout component 😍

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,26 @@
 import React from "react";
 import "./Header.css";
-import SearchIcon from '@material-ui/icons/Search';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt=""
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
 
       <div className="header__search">
         <input type="text" className="header__searchInput" />
-        <SearchIcon className="header__searchIcon"  />
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
-
         <div className="header__option">
           <span className="header__optionLineOne">Hello Guest</span>
           <span className="header__optionLineTwo">Sign in</span>
@@ -34,10 +36,12 @@ function Header() {
           <span className="header__optionLineTwo">Prime</span>
         </div>
 
-        <div className="header__optionBasket">
-            <ShoppingBasketIcon/>
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
             <span className="header__optionLineTwo header__basketCount">0</span>
-        </div>
+          </div>
+        </Link>
       </div>
     </div>
   );
