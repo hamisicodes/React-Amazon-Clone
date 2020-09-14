c7a90a005de63b3e4ec8c0ff718933ca8c37d2b2
hamisicodes <newhamisi@gmail.com>
2020-09-14 18:12:11 +0300

    use React context API to add items to the basket 🧺

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import './Product.css';
+import { useStateValue } from './StateProvider';
 
 function Product({id,title,image,price,rating}) {
+    const [{ basket },dispatch] = useStateValue();
+    console.log('This is the basket =====>>>' , basket);
+    const addToBasket = () =>{
+        // dispatch the item into the data layer
+        
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item:{
+                id:id,
+                title: title,
+                image: image,
+                price: price,
+                rating:rating,
+            },
+        });
+    };
+
     return (
         <div className="product">
             <div className="product__info">
@@ -22,7 +40,7 @@ function Product({id,title,image,price,rating}) {
                 </div>
             </div>
             <img src={image} alt=""/>
-            <button>Add to basket</button>
+            <button onClick={addToBasket}>Add to basket</button>
         </div>
     )
 }
