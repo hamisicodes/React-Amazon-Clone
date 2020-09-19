5263648bddd731c4c109850617a48a3d20f8d198
hamisicodes <newhamisi@gmail.com>
2020-09-19 22:48:07 +0300

    first time deployment to firebase

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -4,7 +4,7 @@ import { useStateValue } from './StateProvider';
 
 function CheckoutProduct({id,title,image,price,rating}) {
 
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket,user }, dispatch] = useStateValue();
 
     const removeFromBasket = () => {
         dispatch({
